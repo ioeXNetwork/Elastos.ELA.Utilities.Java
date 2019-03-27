a1be0377752692916fe28b50dc02b98d97d11296
hungjiunshie <hungjiunshie@kortide.com>
2019-03-27 17:02:52 +0800

    modify asset id

--- src/main/java/org/elastos/ela/Common.java
+++ src/main/java/org/elastos/ela/Common.java
@@ -8,6 +8,7 @@ import javax.xml.bind.DatatypeConverter;
  * Created by nan on 18/1/14.
  */
 public class Common {
-    public final static String SYSTEM_ASSET_ID = "a3d0eaa466df74983b5d7c543de6904f4c9418ead5ffd6d25814234a96db37b0";
+    //public final static String SYSTEM_ASSET_ID = "a3d0eaa466df74983b5d7c543de6904f4c9418ead5ffd6d25814234a96db37b0";
+    public final static String SYSTEM_ASSET_ID = "61ccbfae9f8ce9660a71321041917139cb72cbb85bd105e92f0ed32cb1d1298f";
     public final static byte[] ELA_ASSETID = Utils.reverseBytes(DatatypeConverter.parseHexBinary(SYSTEM_ASSET_ID));
 }
